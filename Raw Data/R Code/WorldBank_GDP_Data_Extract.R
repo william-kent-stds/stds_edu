b0f543d0789e65fd286c3ff258ef4c001c2128e7
will.kent <will.kent@menulog.com>
2019-04-07 18:01:35 +1000

    World Bank Download of GDP values

---
+++ Raw Data/R Code/WorldBank_GDP_Data_Extract.R
@@ -0,0 +1,33 @@
+#install.packages("wbstats")
+library(wbstats)
+
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+#getwd()
+
+if (!dir.exists("../Data Files/World Bank")) {
+  dir.create("../Data Files/World Bank")
+}
+
+# Create cache for world bank data
+world_bank_cache <- wbcache()
+
+# Use wbsearch to find a data set using regular expressions - I think we need NY.GDP.PCAP.PP.CD
+#wbsearch("gdp.*capita", cache = world_bank_cache)
+
+gdp_data <- wb(indicator = "NY.GDP.PCAP.PP.CD")
+summary(gdp_data)
+head(gdp_data)
+
+# There are no duplicate records
+gdp_data %>% 
+  group_by(iso3c, date) %>% 
+  summarise(num_records = n()) %>% 
+  filter(num_records > 1)
+
+# Get only the latest records - for most countries the last year of data is 2017 but for some it is early, also top_n future proofs code if 2018 added
+gdp_2017 <- gdp_data %>% 
+  select(iso3c, date, value) %>% 
+  group_by(iso3c) %>% 
+  top_n(1, date)
+
+write.csv(gdp_2017, "../Data Files/World Bank/GDP.csv", row.names = FALSE)
