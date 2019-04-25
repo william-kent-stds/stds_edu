05dc167a4c05e010f2997630523b92ffc6b7f3e6
justin-htet <htet.n.aung@student.uts.edu.au>
2019-04-25 14:13:30 +1000

    Use ASGS 2016 for ERP
    - update the raw data of ERP csv file with ASGS 2016
    - process raw ERP data by grouping total male, total female, and total for each age band

--- Clean Data/R Code/ABS_ERP_Data.R
+++ Clean Data/R Code/ABS_ERP_Data.R
@@ -1,18 +1,31 @@
-library(readr)
-library(dplyr)
+library(tidyverse)
 library(janitor)
 
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
-erp <- read_csv("../../Raw Data/Data Files/ABS/ERP/ABS_ERP_ASGS_24042019155213908.csv")
+erp <- read_csv("../../Raw Data/Data Files/ABS/ERP/ABS_ERP_ASGS2016_25042019132433480.csv")
 
+names(erp)
 erp <- erp %>% 
   select(-c("MEASURE", "Measure", "SEX_ABS", "AGE", "FREQUENCY", "Frequency", 
-            "TIME", "Flag Codes", "Flags", "REGIONTYPE", "Region Type"))
+            "TIME", "Flag Codes", "Flags", "REGIONTYPE", "Geography Level"))
 
 erp <- erp %>% 
-  rename(SA2_code = ASGS_2011) %>% 
-  clean_names() %>% 
-  filter(age != "All ages")
+  rename(sa2_code = ASGS_2016) %>% 
+  clean_names()
 
-write_csv(erp, "../Data Files/ABS/ERP_SA2.csv")
+erp_by_sex <- erp %>%
+  group_by(sa2_code, sex) %>%
+  summarise(total_value = sum(value)) %>% 
+  spread(sex, total_value) %>% 
+  clean_names()
+
+erp_by_age <- erp %>%
+  group_by(sa2_code, age) %>%
+  summarise(total_value = sum(value)) %>% 
+  spread(age, total_value) %>% 
+  clean_names()
+
+erp <- erp_by_sex %>% left_join(erp_by_age)
+
+write_csv(erp, "../Data Files/ABS/ERP_SA2_2016.csv")
--- Raw Data/Data Files/ABS/ERP/README.md
+++ Raw Data/Data Files/ABS/ERP/README.md
@@ -1,6 +1,6 @@
 ## Info for ABS_ERP_ASGS_24042019155213908.csv
 
 The csv file was downloaded from stat.data.abs.gov.au under
-the theme People -> Population -> ERP by SA2 (ASGS 20100).
+the theme People -> Population -> ERP by SA2 (ASGS 2016) Age and Sex, 2001 Onwards.
 All of the data under NSW with male and female are selected for 
-the year 2016 with 19 age bands at SA2 level.
+the year 2016 with 18 age bands at SA2 level.
