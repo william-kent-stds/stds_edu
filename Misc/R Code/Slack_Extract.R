244988211536dafe4b3b495a986dc0ce92a3753e
will.kent <will.kent@menulog.com>
2019-04-26 17:25:56 +1000

    Update to Slack Extract

--- Misc/R Code/Slack_Extract.R
+++ Misc/R Code/Slack_Extract.R
@@ -3,10 +3,10 @@ library(httr)
 library(jsonlite)
 
 # Go to https://api.slack.com/custom-integrations/legacy-tokens and generate a Legacy token for the UTS MDSI workspace
-# Token = xoxp-42906300790-323427295908-619040841589-1046c48bf2e2433f511800c3c8c76f99
+# Token = To Be Added
 
 # Set up slack integration
-token <- "xoxp-42906300790-323427295908-619040841589-1046c48bf2e2433f511800c3c8c76f99"
+token <- "To Be Added"
 api_url <- paste0("https://slack.com/api/groups.history?token=",token,"&channel=GH834L2RK&pretty=1")
 
 # Get request to Slack and store JSON response
